f427b035e9b70491912045d7cdc0da689bfe5902
Pawan Kumar <pawan.kumar@intelegencia.com>
2023-08-15 10:47:20 +0530

    my first block

---
+++ plugins/first-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6a5a9ca8f409203cc91d');
---
+++ plugins/first-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd9500701c769aa5f8207');
